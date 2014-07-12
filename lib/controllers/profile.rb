7925a02e6a3224d6ff57f488858417e63852cd34
Prateek Agarwal <prat0318@gmail.com>
2014-07-12 08:21:15 -0700

    working results on frontend

--- lib/controllers/profile.rb
+++ lib/controllers/profile.rb
@@ -18,7 +18,35 @@ class DreamOn::Controllers::Profile < Sinatra::Base
         open_decriptor = open("https://www.linkedin.com/in/#{params[:id]}")
       elsif params[:url]
         params[:url].gsub!('http', 'https') if params[:url].scan('https').empty?
-        open_decriptor = open("#{params[:url].strip}")
+				arr = params[:url].split(",,")
+
+				puts "-------------"
+				arr.map! do |x|
+					arr1 = x.split("--");
+					parse(open(arr1[0].strip), arr1[1])
+				end
+        # open_decriptor = open("#{params[:url].strip}")
+				# process and group arr and send back
+				puts arr
+				arr1 = process(arr)
+				puts "--=-=-=-=-="
+				puts arr1
+				arr1.each do |k,v|
+					v.each do |k,degree|
+						degree.each do |d|
+							d[1] = []
+							arr.each do |match|
+								puts "+++++"
+								puts match[:educations].to_json, d[0]
+								if match[:educations].to_json.downcase.include? d[0].downcase
+									d[1] << match[:number]
+								end
+							end
+						end
+					end
+				end
+				puts arr1
+				body arr1.to_json
       end
     rescue RuntimeError
       return not_found("dude! use https url.")
@@ -26,6 +54,41 @@ class DreamOn::Controllers::Profile < Sinatra::Base
       return not_found("User not found: #{params[:id]}")
     end
 
+		# dict = parse(open_decriptor)
+
+    # body education_hash.to_json
+  end
+end
+
+def process(arr)
+	major = {"graduate"=>[], "undergrad"=>[], "rest"=>[]}
+	school = {"graduate"=>[], "undergrad"=>[], "rest"=>[]}
+	arr.each do |edu_dict|
+		edu_arr = edu_dict[:educations]
+		edu_arr.each do |edu|
+			next unless edu[:degree]
+			if edu[:degree].starts_with?("M") || edu[:degree].starts_with?("P")
+				major["graduate"] << edu[:major] if edu[:major]
+				school["graduate"] << edu[:name] if  edu[:name]
+				elsif edu[:degree].starts_with?("B")
+				major["undergrad"] << edu[:major] if edu[:major]
+				school["undergrad"] << edu[:name] if  edu[:name]
+			else
+				major["rest"] << edu[:major] if edu[:major] 
+				school["rest"] << edu[:name] if  edu[:name]
+			end
+		end
+	end
+		major["graduate"] = (major["graduate"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		major["undergrad"] = (major["undergrad"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		major["rest"] = (major["rest"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		school["graduate"] = (school["graduate"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		school["undergrad"] = (school["undergrad"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		school["rest"] = (school["rest"].group_by(&:capitalize).map {|k,v| [k, v.length]}).sort_by{|x| -x[1]}[0..1]
+		{:major => major, :school => school}
+end
+
+def parse(open_decriptor, num)
     doc = Nokogiri::HTML(open_decriptor)
     education_hash = {}
 
@@ -48,7 +111,6 @@ class DreamOn::Controllers::Profile < Sinatra::Base
       single_education[:major] = major if major      
       education_hash[:educations] << single_education
     end
-
-    body education_hash.to_json
-  end
+    education_hash[:number] = num
+		education_hash
 end
